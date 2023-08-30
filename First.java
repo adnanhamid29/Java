fb770b2fb065928ddc5f755d9c1e89bb96d826ce
unknown <adnanhamid29@gmail.com>
2023-08-30 23:22:36 +0530

    addition

--- First.java
+++ First.java
@@ -2,5 +2,10 @@ class First{
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        int a, b ,SUM;
+        a=10;
+        b=20;
+        SUM=a+b;
+        System.out.println(SUM);
     }
-}
+}           
