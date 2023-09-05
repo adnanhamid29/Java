7178f18e52711d3fb0496c6d39a0dc6002d708b2
unknown <adnanhamid29@gmail.com>
2023-09-05 23:34:47 +0530

    taking input from user

---
+++ class1.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+;public class class1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter first number");
+       int num1=sc.nextInt();
+       System.out.println("enter second number");
+       int num2= sc.nextInt();
+       int sum= num1+ num2;
+       System.out.println("the sum of "+num1+" and "+num2+" is "+sum);
+       
+
+}
+}
